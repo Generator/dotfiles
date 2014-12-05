cbf375a426b8e9745def56a63d48759c5e23afa8
aj <aj@localhost.lan>
2014-12-05 20:42:36 +0000

    / ‘zsh/zsh/prompt.zsh’

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -98,7 +98,7 @@ antigen_start(){
 	
 	# Load the oh-my-zsh's library.
 	#antigen use oh-my-zsh
-	antigen use prezto
+	#antigen use prezto
 	
 	# Bundles from the default repo (robbyrussell's oh-my-zsh).
 	#antigen bundle git
