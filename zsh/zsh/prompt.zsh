adb0a72e63e11d5609583771c5da7fb33e96292e
Generator <american.jesus.pt@gmail.com>
2015-09-11 23:28:54 +0100

    improved zshrc history options

--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -60,6 +60,7 @@ if ! zgen saved; then
     zgen oh-my-zsh plugins/fasd
     zgen load unixorn/autoupdate-zgen
     zgen load zsh-users/zsh-syntax-highlighting
+    zgen load zsh-users/zsh-completions
     zgen load tarruda/zsh-autosuggestions
     zgen load zsh-users/zsh-history-substring-search
     zgen load Tarrasch/zsh-bd
