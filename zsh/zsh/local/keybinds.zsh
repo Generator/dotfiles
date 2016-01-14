c2ef5245d94e1164cd85f2f43db3661430758862
Generator <american.jesus.pt@gmail.com>
2016-01-14 16:11:34 +0000

    zsh: more changes

--- zsh/zsh/local/keybinds.zsh
+++ zsh/zsh/local/keybinds.zsh
@@ -1,48 +1,29 @@
 #------------------------------
 # key binds
 #------------------------------
-
-# create a zkbd compatible hash;
-# to add other keys to this hash, see: man 5 terminfo
-typeset -A key
-
-key[Home]=${terminfo[khome]}
-
-key[End]=${terminfo[kend]}
-key[Insert]=${terminfo[kich1]}
-key[Delete]=${terminfo[kdch1]}
-key[Up]=${terminfo[kcuu1]}
-key[Down]=${terminfo[kcud1]}
-key[Left]=${terminfo[kcub1]}
-key[Right]=${terminfo[kcuf1]}
-key[PageUp]=${terminfo[kpp]}
-key[PageDown]=${terminfo[knp]}
-
-# setup key accordingly
-[[ -n "${key[Home]}"    ]]  && bindkey  "${key[Home]}"    beginning-of-line
-[[ -n "${key[End]}"     ]]  && bindkey  "${key[End]}"     end-of-line
-[[ -n "${key[Insert]}"  ]]  && bindkey  "${key[Insert]}"  overwrite-mode
-[[ -n "${key[Delete]}"  ]]  && bindkey  "${key[Delete]}"  delete-char
-[[ -n "${key[Up]}"      ]]  && bindkey  "${key[Up]}"      up-line-or-history
-[[ -n "${key[Down]}"    ]]  && bindkey  "${key[Down]}"    down-line-or-history
-[[ -n "${key[Left]}"    ]]  && bindkey  "${key[Left]}"    backward-char
-[[ -n "${key[Right]}"   ]]  && bindkey  "${key[Right]}"   forward-char
+bindkey "^J" self-insert
+bindkey "\e[3~" delete-char
+bindkey '^[[1;3C' forward-word
+bindkey '^[[1;3D' backward-word
+bindkey '\e[1~' beginning-of-line
+bindkey '\e[4~' end-of-line
 bindkey '\e[H' beginning-of-line
 bindkey '\e[F' end-of-line
-
-# Finally, make sure the terminal is in application mode, when zle is
-# active. Only then are the values from $terminfo valid.
-if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
-    function zle-line-init () {
-        printf '%s' "${terminfo[smkx]}"
-    }
-    function zle-line-finish () {
-        printf '%s' "${terminfo[rmkx]}"
-    }
-    zle -N zle-line-init
-    zle -N zle-line-finish
+bindkey '\e[7~' beginning-of-line
+bindkey '\e[8~' end-of-line
+bindkey '^[[5~' up-line-or-history
+bindkey '^[[6~' down-line-or-history
+bindkey '^[[A' history-search-backward
+bindkey '^[[B' history-search-forward
+bindkey ' ' magic-space
+bindkey '^[[Z' reverse-menu-complete
+bindkey -s '^L' "|$PAGER"
+bindkey '^Q' quote-line
+bindkey -s '^N' '> /dev/null 2>&1\n'
+# bind only if fzf installed
+if which fzf >/dev/null; then
+   bindkey '^R' fzf-history-widget
+else
+   echo "fzf not found"
+   bindkey '^R' history-incremental-search-backward
 fi
-
-# History search
-[[ -n "${key[PageUp]}"   ]]  && bindkey  "${key[PageUp]}"    history-beginning-search-backward
-[[ -n "${key[PageDown]}" ]]  && bindkey  "${key[PageDown]}"  history-beginning-search-forward
--- zsh/zsh/prompt.zsh
+++ zsh/zsh/prompt.zsh
@@ -1,126 +1,5 @@
-# Reset prompt
-#if [[ "$OSTYPE" == "linux"* ]]; then
-#prompt off
-#fi
-
-# Change xterm to xterm-256color
-case $TERM in
-		xterm)
-		export TERM=xterm-256color
-		;;
-esac
-
-# URxvt I-Beam cursor
-case $TERM in 
-    rxvt-unicode*)
-    echo -e -n "\x1b[\x35 q"
-esac
-
 #------------------------------
-# Prompt
-#------------------------------
-setprompt () {
-
-		# Create new line
-		prompt_newline=$'\n%{\r%}'	
-
-		# load some modules
-		autoload -U colors zsh/terminfo # Used in the colour alias below
-		colors
-		setopt prompt_subst
-
-		##
-		if [[ $UID != 0 ]]; then
-		UIDC=$'%{\e[1;36m%}'
-		else
-		UIDC=$'%{\e[0;31m%}%B'
-		fi
-
-		# set the prompt
-		PROMPT='[${Blue}%~%{$reset_color%}]%{$reset_color%}
-%B${UIDC}%#%{$fg[black]%}>%b%{$reset_color%}'
-		RPROMPT='${BBlack}%B%n@%M%b%{$reset_color%}'
-}
-
-# Prompt
-#PROMPT='[%{$fg[blue]%}%n%{$reset_color%}][%{$fg[yellow]%}%~%{$reset_color%}]${vcs_info_msg_0_}
-#%B%{$fg[black]%}>%b%{$fg[green]%}>%B%{$fg[green]%}>%b%{$reset_color%}'
-
-#------------------------------
-# zgen
-#------------------------------
-zgen_start(){
-# load zgen
-source ~/.zsh/framework/zgen/zgen.zsh
-
-# check if there's no init script
-if ! zgen saved; then
-    echo "Creating a zgen save"
-
-    zgen oh-my-zsh
-
-    # plugins
-    zgen oh-my-zsh plugins/git
-    zgen oh-my-zsh plugins/extract
-    zgen oh-my-zsh plugins/command-not-found
-    zgen oh-my-zsh plugins/fasd
-    zgen load zsh-users/zsh-syntax-highlighting
-    zgen load zsh-users/zsh-history-substring-search
-    zgen load unixorn/autoupdate-zgen
-    zgen load tarruda/zsh-autosuggestions
-    zgen load Tarrasch/zsh-bd
-    zgen load rimraf/k
-
-    # bulk load
-    zgen loadall <<EOPLUGINS
-EOPLUGINS
-    # ^ can't indent this EOPLUGINS
-
-    # completions
-    zgen load zsh-users/zsh-completions src
-
-    # theme
-    #zgen oh-my-zsh themes/arrow
-    zgen load caiogondim/bullet-train-oh-my-zsh-theme bullet-train
-
-    # save all to init script
-    zgen save
-fi
-}
-
-#------------------------------
-# zplug
-#------------------------------
-zplug_start(){
-source ~/.zplug/zplug
-
-    # Local pluginszplug "~/.zsh/local", from:local
-    zplug "~/.zsh/local", from:local, nice:-20, of:"*.zsh"
-
-    zplug "plugins/git", from:oh-my-zsh
-    zplug "plugins/extract", from:oh-my-zsh
-    zplug "plugins/command-not-found", from:oh-my-zsh
-    zplug "plugins/fasd", from:oh-my-zsh
-    zplug "Tarrasch/zsh-bd"
-    zplug "rimraf/k"
-    zplug "zsh-users/zsh-syntax-highlighting", nice:10
-    zplug "zsh-users/zsh-history-substring-search", nice:11
-    zplug "tarruda/zsh-autosuggestions", nice:12
-
-    # Theme
-    zplug "caiogondim/bullet-train-oh-my-zsh-theme"
-
-# Install plugins if there are plugins that have not been installed
-if ! zplug check --verbose ; then
-   zplug install --verbose
-fi
-
-# Then, source plugins and add commands to $PATH
-zplug load
-}
-zplug_start
-#------------------------------
-# Options
+# Options Before
 #------------------------------
 
 # Options bullet-train-oh-my-zsh-theme
@@ -129,16 +8,79 @@ BULLETTRAIN_HG_SHOW=false
 
 # Disable oh-my-zsh autoupdate
 DISABLE_AUTO_UPDATE=true
+
+# zsh-syntax-highlighting
+if [[ "$OSTYPE" != "cygwin"* ]]; then
+   ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor root)
+else
+   ZSH_HIGHLIGHT_HIGHLIGHTERS=()
+fi
+
+
+# zsh-completion-generator
+GENCOMPL_PY=python2
+GENCOMPL_FPATH=$HOME/.zsh/complete
+
+# enable agent forwarding
+zstyle :omz:plugins:ssh-agent agent-forwarding on
+zstyle :omz:plugins:ssh-agent quiet yes
+zstyle :omz:plugins:ssh-agent identities id_rsa id_rsa_aur
+
 #------------------------------
-# Select prompt
+# zplug
 #------------------------------
-#case $TERM in
-#		xterm*|rxvt*|screen*|tmux*)
-#        #zgen_start
-#        zplug_start
-#				#setprompt
-#				;;
-#		linux)
-#				setprompt
-#				;;
-#esac
+source ~/.zplug/zplug
+
+    # Local plugins
+    zplug "~/.zsh/local", from:local, nice:-20, of:"*.zsh"
+
+    # oh-my-zsh plugins
+    zplug "plugins/command-not-found", from:oh-my-zsh
+    zplug "plugins/extract", from:oh-my-zsh
+    zplug "plugins/fasd", from:oh-my-zsh
+    zplug "plugins/fancy-ctrl-z", from:oh-my-zsh
+    zplug "plugins/git", from:oh-my-zsh
+
+    # github plugins
+    zplug "b4b4r07/enhancd", of:enhancd.sh
+    zplug "atweiden/fzf-extras", of:fzf-extras.sh
+    zplug "Tarrasch/zsh-bd"
+    zplug "rimraf/k"
+    zplug "RobSis/zsh-completion-generator"
+    zplug "zsh-users/zsh-completions"
+    zplug "zsh-users/zsh-syntax-highlighting", nice:10
+    zplug "zsh-users/zsh-history-substring-search", nice:11
+    zplug "tarruda/zsh-autosuggestions", nice:12
+
+    # Theme
+    zplug "caiogondim/bullet-train-oh-my-zsh-theme", if:"[[ $TERM != linux ]]"
+    zplug "~/.zsh/local/theme", from:local, nice:-19, of:"simple-prompt.zsh-theme", if:"[[ $TERM == linux ]]"
+
+# Install plugins if there are plugins that have not been installed
+if ! zplug check --verbose ; then
+   zplug install --verbose
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load
+
+#------------------------------
+# Options After
+#------------------------------
+
+#------------------------------
+# Enable autosuggestions automatically
+#------------------------------
+zle-line-init() {
+	 zle autosuggest-start
+	}
+zle -N zle-line-init
+
+# use ctrl+t to toggle autosuggestions(hopefully this wont be needed as
+# zsh-autosuggestions is designed to be unobtrusive)
+bindkey '^T' autosuggest-toggle
+# Accept suggestions without leaving insert mode
+bindkey '^f' vi-forward-word
+# or
+# bindkey '^f' vi-forward-blank-word
+AUTOSUGGESTION_ACCEPT_RIGHT_ARROW=1
