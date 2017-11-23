57734a6a1bb84a949f6181bb1f7ab972ec70f536
Generator <american.jesus.pt@gmail.com>
2017-11-23 17:34:07 +0000

    update ssh-agent.zsh

--- zsh/zsh/local/ssh-agent.zsh
+++ zsh/zsh/local/ssh-agent.zsh
@@ -4,3 +4,8 @@
 # GNOME Keyring
 #eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
 #export SSH_AUTH_SOCK
+
+# Tmux SSH Agent
+if [ -z "$DESKTOP_SESSION" ]; then
+eval $(tmux show-env -s |grep '^SSH_')
+fi
